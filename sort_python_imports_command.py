42c89d664dc952b99eb0dd5f0bdc498714f7d7dc
minhnguyent546 <minhnguyent546@gmail.com>
2024-07-15 10:50:46 +0700

    feat: handle inline comment in sort_python_imports_command.py
    Inline comment associating with import statement is now remain after sorting

--- sort_python_imports_command.py
+++ sort_python_imports_command.py
@@ -2,6 +2,7 @@
 
 import ast
 from functools import cmp_to_key
+from typing import List, Optional, Tuple
 
 import sublime
 import sublime_plugin
@@ -17,8 +18,17 @@ class SortPythonImportsCommand(sublime_plugin.TextCommand):
         regions = self.view.find_by_selector('meta.statement.import')
         if not regions:
             return
-        # import statements can have comment associated with them, so we need to expand the region
+
+        # import statements can have inline comment associated with them,
+        # so we need to expand the region to cover that comment
         regions = [self.view.line(region) for region in regions]
+
+        assoc_comments: List[Optional[str]] = [None for _ in range(len(regions))]
+        for idx, region in enumerate(regions):
+            current_line = self.view.substr(self.view.line(region))
+            comment_symb_idx = current_line.rfind('#')
+            if comment_symb_idx != -1:
+                assoc_comments[idx] = current_line[comment_symb_idx:]
 
         # insert two lines after import statements
         last_import_row = self.view.rowcol(regions[-1].end())[0]
@@ -52,19 +62,20 @@ class SortPythonImportsCommand(sublime_plugin.TextCommand):
             assert self.line_content(cur_row).strip() == ''
             self.view.erase(edit, self.view.full_line(self.view.text_point(cur_row, 0)))
 
-        import_section_regions = []
+        import_section_regions: List[List[Tuple[sublime.Region, Optional[str]]]] = []
         for idx in range(len(regions)):
             region = regions[idx]
             if idx == 0 or self.view.rowcol(region.begin())[0] > self.view.rowcol(regions[idx - 1].end())[0] + 1:
-                import_section_regions.append([region])
+                import_section_regions.append([(region, assoc_comments[idx])])
             else:
-                import_section_regions[-1].append(region)
+                import_section_regions[-1].append((region, assoc_comments[idx]))
 
         for import_section_region in reversed(import_section_regions):
             section_content = self.view.substr(sublime.Region(
-                import_section_region[0].begin(),
-                import_section_region[-1].end()),
+                import_section_region[0][0].begin(),
+                import_section_region[-1][0].end()),
             )
+            section_assoc_comments = [assoc_comment for _, assoc_comment in import_section_region]
 
             # TODO: assume that section_content contains only import statements
             # TODO: ast.parse will not take into account comments (i.e. all comments will be gone)
@@ -81,19 +92,22 @@ class SortPythonImportsCommand(sublime_plugin.TextCommand):
                 node.names = sorted(node.names, key=lambda alias: alias.name)
 
             # sort import statements
-            tree.body = sorted(tree.body, key=cmp_to_key(node_cmp))
+            tree.body, section_assoc_comments = zip(*sorted(zip(tree.body, section_assoc_comments), key=cmp_to_key(node_cmp)))
 
             # remove duplicates
             new_body = []
+            new_section_assoc_comments = []
             for idx in range(len(tree.body)):
                 if idx == 0 or ast.dump(tree.body[idx]) != ast.dump(tree.body[idx - 1]):
                     new_body.append(tree.body[idx])
+                    new_section_assoc_comments.append(section_assoc_comments[idx])
             tree.body = new_body
+            section_assoc_comments = new_section_assoc_comments
 
-            sorted_section_content = unparse_imports(tree)
+            sorted_section_content = unparse_imports(tree, section_assoc_comments)
             self.view.replace(
                 edit,
-                sublime.Region(import_section_region[0].begin(), import_section_region[-1].end()),
+                sublime.Region(import_section_region[0][0].begin(), import_section_region[-1][0].end()),
                 sorted_section_content,
             )
 
@@ -113,7 +127,7 @@ class SortPythonImportsCommand(sublime_plugin.TextCommand):
                 return True
         return False
 
-def unparse_imports(tree: ast.Module, tab_size: int = 4) -> str:
+def unparse_imports(tree: ast.Module, assoc_comments: List[Optional[str]], tab_size: int = 4) -> str:
     source = []
     tabs = " " * tab_size
 
@@ -125,7 +139,8 @@ def unparse_imports(tree: ast.Module, tab_size: int = 4) -> str:
         {'num_aliases': 6, 'limit': 44},
         {'num_aliases': 7, 'limit': -1},
     ]
-    for node in tree.body:
+    assert len(tree.body) == len(assoc_comments)
+    for idx, node in enumerate(tree.body):
         if isinstance(node, ast.Import):
             prefix = 'import '
         elif isinstance(node, ast.ImportFrom):
@@ -150,11 +165,13 @@ def unparse_imports(tree: ast.Module, tab_size: int = 4) -> str:
             prefix += f'(\n{tabs}'
             all_aliases = all_aliases.replace(', ', f',\n{tabs}')
             all_aliases += ',\n)'
-
+        if assoc_comments[idx] is not None:
+            all_aliases += '  ' + assoc_comments[idx]
         source.append(prefix + all_aliases)
     return '\n'.join(source)
 
 def node_cmp(lhs, rhs) -> int:
+    lhs, rhs = lhs[0], rhs[0]
     if not isinstance(lhs, (ast.Import, ast.ImportFrom)):
         raise ValueError(f'Expected lhs to be an import or import from statement, got: {ast.dump(lhs)}')
     if not isinstance(rhs, (ast.Import, ast.ImportFrom)):
